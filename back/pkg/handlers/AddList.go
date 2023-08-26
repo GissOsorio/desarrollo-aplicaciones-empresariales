47d593fc8146d4344f9399ea74c31ea74b940d74
Gisela Osorio <giss_ot@hotmail.com>
2023-08-25 23:05:41 -0500

    Update AddList

--- back/pkg/handlers/AddList.go
+++ back/pkg/handlers/AddList.go
@@ -11,14 +11,24 @@ import (
 )
 
 func (h handler) AddList(w http.ResponseWriter, r *http.Request) {
+        // Enable CORS by setting the appropriate headers
+        w.Header().Set("Access-Control-Allow-Origin", "*")            // Allow requests from any origin
+        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS") // Allow POST and OPTIONS methods
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")  // Allow Content-Type header
     
+        // Handle preflight OPTIONS request (sent by browsers before the actual POST request)
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
     defer r.Body.Close()
     
     body, err := ioutil.ReadAll(r.Body)
 
     if err != nil {
         log.Fatalln(err)
-        w.WriteHeader(500)
+        w.WriteHeader(http.StatusInternalServerError)
         return
     }
     var list models.List
@@ -30,13 +40,11 @@ func (h handler) AddList(w http.ResponseWriter, r *http.Request) {
     err = h.DB.QueryRow(queryStmt, &list.Id, &list.Date, &list.UserId, &list.Name, &list.Status).Scan(&list.Id)
     if err != nil {
         log.Println("failed to execute query", err)
-        w.WriteHeader(500)
+        w.WriteHeader(http.StatusInternalServerError)
         return
     }
 
     w.Header().Add("Content-Type", "application/json")
-    enableCors(&w)
-    w.Header().Set("Referer", "")
     w.WriteHeader(http.StatusCreated)
     json.NewEncoder(w).Encode("Created")
 }
